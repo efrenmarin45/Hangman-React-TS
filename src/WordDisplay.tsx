2c46669436ca7aefa7da00c5fa0264b69272c95b
Efren_Marin <efren45marin@gmail.com>
2023-02-27 23:25:27 -0800

    Included word field

--- src/WordDisplay.tsx
+++ src/WordDisplay.tsx
@@ -1,5 +1,27 @@
-const WordDisplay = () => {
+import styled from "styled-components";
 
+const Word = styled.div`
+    display: flex;
+    gap: .25em;
+    font-size: 6rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-family: monospace;
+`
+
+const WordDisplay = () => {
+    const testWord = "apple";
+    const userLetters = ["b", "a", "t", "e", "l"];
+    return(
+        <Word>
+            {testWord.split("").map((letter, index) => (
+                <span style={{ borderBottom: ".1em solid black"}} key={index}>
+                    <span style={{visibility: userLetters.includes(letter) ? "visible" : "hidden"}}>{letter}
+                    </span>
+                </span>
+            ))}
+        </Word>
+    )
 };
 
 export default WordDisplay;
